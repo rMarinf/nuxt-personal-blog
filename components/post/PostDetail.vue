70258355939473267677e0645e12be67e9351e83
rMarinf <rmarinfdez@gmail.com>
2020-05-18 18:21:45 +0200

    Remove author section

--- components/post/PostDetail.vue
+++ components/post/PostDetail.vue
@@ -20,26 +20,6 @@
               </div>
             </div>
 
-            <!-- AUTHOR -->
-            <div class="about-author d-flex p-4 bg-light">
-              <div class="bio mr-5">
-                <img
-                  src="images/person_1.jpg"
-                  alt="Image placeholder"
-                  class="img-fluid mb-4"
-                />
-              </div>
-              <div class="desc">
-                <h3>Roberto Marín</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Ducimus itaque, autem necessitatibus voluptate quod mollitia
-                  delectus aut, sunt placeat nam vero culpa sapiente consectetur
-                  similique, inventore eos fugit cupiditate numquam!
-                </p>
-              </div>
-            </div>
-
             <!-- COMMENTS -->
             <!-- TODO DISQUS -->
           </div>
